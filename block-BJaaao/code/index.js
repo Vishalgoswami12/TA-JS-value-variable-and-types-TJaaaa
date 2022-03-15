8cf1bc604794d68a9aeb38209c8c41b1d0106b76
vishalgoswami12 <vishalgoswami2259@gmail.com>
2022-03-15 12:21:12 +0530

    Learning Utility Function

--- block-BJaaao/code/index.js
+++ block-BJaaao/code/index.js
@@ -16,6 +16,10 @@ let num2 = 32;
 - Alert `The value of num1 is [num1] and the value of num2 is [num2]`
 - Alert `The sum of [num1] and [num2] is [num1 + num1]`
 */
+let language = "Javascript";
+alert(`I am learning ${language}`);
+alert(`The value of num1 is ${num1} and the value of num2 is ${num2}`);
+alert(`The sum of ${num1} and ${num2} is ${num1 + num2}`);
 
 /*
 2. Do the following using `alert`, `prompt` and `confirm` and `console`
@@ -33,6 +37,17 @@ let num2 = 32;
     Profession: [userProfession]` (it should be in three different lines)
 */
 
+let userName = prompt("What is your name?");
+let userProfession = prompt("What is your Profession?");
+let isAdult = confirm("Are you adult?");
+alert(`I am ${userName} a ${userProfession}`);
+console.log(userName, userProfession);
+alert(
+  `Username: ${userName}\n Adult: ${isAdult}\n Profession: ${userProfession}`
+);
+console.log(
+  `username:${userName},\n Adult:${isAdult}, \n Profession:${userProfession} `
+);
 /*
 . Addition using prompt
 
@@ -43,3 +58,11 @@ let num2 = 32;
   - Log using console.log [numA - numB]
   - Log using console.log [numA * numB]
 */
+let numA = prompt("What is value of A?");
+let numB = prompt("What is value of B?");
+Number("20");
+Number("15");
+alert(numA - numB);
+alert(numA * numB);
+console.log(numA - numB);
+console.log(numA * numB);
