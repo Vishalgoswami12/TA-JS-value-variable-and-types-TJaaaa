df457b2e0bc33a3c304d1a15e3cf6e5c1a1b13ab
vishalgoswami12 <vishalgoswami2259@gmail.com>
2022-03-21 20:13:20 +0530

    Assignment3

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -1,8 +1,11 @@
 /*
 🎖 Write a program to calculate the total price of your phone purchase. With these conditions
  * [ ] You will keep purchasing phones (hint: loop!) until you run out of bank balance.
+
  * [ ] You'll also buy accessories for each phone as long as your purchase amount is below your spending threshold.
+
  * [ ] After you've calculated your purchase amount, add in the tax, then print out the calculated purchase amount, properly formatted like ($334.76).
+
  * [ ] Finally, check the amount against your bank account balance to see if you can afford it or not.
 */
 
@@ -13,6 +16,22 @@ const ACCESSORY_PRICE = 9.99;
 
 var bank_balance = 303.91;
 var amount = 0;
-// your code goes here
 
+// your code goes here
+// let purchaseAmount = 0;
+let i = PHONE_PRICE;
+for (i; i <= bank_balance; i = i + PHONE_PRICE) {
+  if (i < SPENDING_THRESHOLD) {
+    console.log("Purchase");
+    i = i + ACCESSORY_PRICE;
+  }
+}
+
+var tax = i * TAX_RATE;
+var total = i + tax;
+
+console.log(`$ ${total.toFixed(2)}`);
+if (total > bank_balance) {
+  console.log("Insufficient balance");
+}
 // ⛑ Answer of the above will `$334.76`.
